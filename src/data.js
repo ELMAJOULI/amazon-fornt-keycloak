49f68952ff985a73ed20639fb9a3c6feec2788b8
ELMAJOULI ABDELWAHED <aelmajouli@gmail.com>
2020-11-03 23:29:13 +0100

    amazon clone v1

--- src/data.js
+++ src/data.js
@@ -9,43 +9,43 @@ export const data =[
     {
         id :101,
         title:"The lean Startup: How Constant Innovation Creates ",
-        price:106,
-        image:"https://m.media-amazon.com/images/I/51JvqroQdWL._SY346_.jpg",
+        price:326,
+        image:"https://images-eu.ssl-images-amazon.com/images/G/02/uk-pc/certifiedrefurbished/redesign/UK_Amazon-Renewed-1052644_20-6-2017_670x700-v11._V507211970_.jpg",
         rating:5
     },
     {
         id :102,
-        title:"The lean Startup: How Constant Innovation Creates ",
-        price:106,
-        image:"https://m.media-amazon.com/images/I/51JvqroQdWL._SY346_.jpg",
-        rating:5
+        title:"Introducing Echo Buds | Wireless earbuds with immersive sound, active noise reduction and Alexa  ",
+        price:296,
+        image:"https://images-na.ssl-images-amazon.com/images/I/5119wd-LkiL._AC_SL1000_.jpg",
+        rating:3
     },
     {
         id :103,
-        title:"The lean Startup: How Constant Innovation Creates ",
-        price:106,
-        image:"https://m.media-amazon.com/images/I/51JvqroQdWL._SY346_.jpg",
-        rating:5
+        title:"Echo Show 5 –Stay in touch with the help of Alexa, Black ",
+        price:96,
+        image:"https://images-na.ssl-images-amazon.com/images/I/61fPnzNnHZL._AC_SL1000_.jpg",
+        rating:4
     },
     {
         id :104,
-        title:"The lean Startup: How Constant Innovation Creates ",
-        price:106,
-        image:"https://m.media-amazon.com/images/I/51JvqroQdWL._SY346_.jpg",
+        title:"Calvin Klein CK ONE Unisex Eau de Toilette, 300ml ",
+        price:450,
+        image:"https://images-na.ssl-images-amazon.com/images/I/71jQ8NwBWHL._AC_SL1500_.jpg",
         rating:5
     },
     {
         id :105,
-        title:"The lean Startup: How Constant Innovation Creates ",
+        title:"ELEPHAS Projector, GC333 Portable Projector with 4500 Lumens and Full HD 1080p, 180” Display and 50000 Hours Lamp Life LED Video Projector, Compatible with USB/HD/Sd/Av/VGA for Home Theater, White  ",
         price:106,
-        image:"https://m.media-amazon.com/images/I/51JvqroQdWL._SY346_.jpg",
-        rating:5
+        image:"https://images-na.ssl-images-amazon.com/images/I/6181UW7sueL._AC_SL1500_.jpg",
+        rating:4
     },
     {
         id :106,
-        title:"The lean Startup: How Constant Innovation Creates ",
+        title:"Wind Took Women's Wallet Ladies Large Capacity Purse Long with Multiple Card Slots and Zipper Coin Pocket Card Holders Phone Pocket ",
         price:106,
-        image:"https://m.media-amazon.com/images/I/51JvqroQdWL._SY346_.jpg",
-        rating:5
+        image:"https://images-na.ssl-images-amazon.com/images/I/81qyJJuxbUL._AC_SL1500_.jpg",
+        rating:3
     }
 ]
